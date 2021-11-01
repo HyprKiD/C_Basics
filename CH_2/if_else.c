b815ec3c994f3b52b5ae37b940056ee0bc416e30
HyperKiD JoDD <89870945+HyprKiD@users.noreply.github.com>
2021-11-01 21:47:11 +0530

    Create if_else.c

---
+++ CH_2/if_else.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+int main(){
+   int n;
+    printf("This is a program to show working of if-else\n");
+    printf("Value daloo:");
+    scanf("%d", &n);
+    if (75<3){
+      printf("Kaam krta h aaise");   
+   }
+
+    else{
+      printf("not possible");
+   }
+   return 0;
+}
