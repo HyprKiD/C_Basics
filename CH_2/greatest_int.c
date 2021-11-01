28222d655e54fd7dd80fc732d0a71e1118e050f3
HyperKiD JoDD <89870945+HyprKiD@users.noreply.github.com>
2021-11-01 21:53:46 +0530

    Create greatest_int.c

---
+++ CH_2/greatest_int.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+
+int main(){
+    float a,b,c,d;
+    printf("Program to find the greatest of four numbers entered by the user\n");
+  
+    printf("Enter first number:\n");
+    scanf("%f", &a);
+
+    printf("Enter second number:\n");
+    scanf("%f", &b);
+
+    printf("Enter third number:\n");
+    scanf("%f", &c);
+    
+    printf("Enter fourth number:\n");
+    scanf("%f", &d);
+   if ()
+   {tmpA
+       /* code */
+   }
+    
+    
+    
+    
+    return 0;
+}
